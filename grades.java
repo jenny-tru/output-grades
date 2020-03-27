c7313dd2457fd3cb9eaa0484ad8bf9a981ff6810
jenny-tru <jennytru@DESKTOP-PHUSTMO.localdomain>
2020-03-27 15:26:16 -0400

    :tada: fixed grading errors

--- grades.java
+++ grades.java
@@ -1,133 +1,108 @@
-package javasandbox;
+package com.hybrid.dev.jenny.sandbox;
 
 import java.io.*;
 import java.util.ArrayList;
+/*Create a 10-row record file using delimiter "^"
+Student First Name^Student Last Name^Student Grade(Numeric)
+Read file
+Parse file
+Key: 0-64=F, 65-74=D, 75-79=C, 80-89=B, 90-100=A    
+Output 1
+             Basic output of all data in numbered list
+             Group Title
+             1. Full Name
+Output 2
+             Metrics of Categorize groups --A, B, C, D, F
+             Total Rows in File:
+             Students with A Grade: 3 */
 
-public class GradesV1 {
-	//categorize grade groups
+public class GradesV3 {
+	// categorize grade groups
 	ArrayList<String> aList = new ArrayList<String>();
 	ArrayList<String> bList = new ArrayList<String>();
 	ArrayList<String> cList = new ArrayList<String>();
 	ArrayList<String> dList = new ArrayList<String>();
 	ArrayList<String> fList = new ArrayList<String>();
 
-	public void readMe() {
-		// open the file
+	public void processFile(){
+		readFile();
+		System.out.println("=================================================");
+		displayStudentGroup();
+		System.out.println("=================================================");
+	}
+	
+	public void readFile() {
 		String filePath = "C:\\Data\\Java\\CS\\Jenny\\Email\\Grades.txt";
 		try {
-			// read file
 			FileReader fr = new FileReader(filePath);
 			BufferedReader br = new BufferedReader(fr);
 			String line = "";
-			int rows = 0;
-			//amount of rows in file
-			for (int fileRow = 1; (line = br.readLine()) != null; fileRow++) {
-				rows++;
+			int numStudents = 0;
+			//row reads through each row of text file
+			for (int row = 1; (line = br.readLine()) != null; row++) {
+				numStudents++;
 				parseFile(line);
-			}br.close();
-			displayStudentGroup();
-
-			System.out.println("Output 2");
-			System.out.println("----------------------------------------");
-			System.out.println("Students with A Grade: " + aList.size());
-			System.out.println("Students with B Grade: " + bList.size());
-			System.out.println("Students with C Grade: " + cList.size());
-			System.out.println("Students with D Grade: " + dList.size());
-			System.out.println("Students with F Grade: " + fList.size());
-			System.out.println("----------------------------------------");
-			System.out.println("Total rows in file: " + rows);
-			System.out.println("----------------------------------------");
-			System.out.println("----------------------------------------");
-		} catch (IOException E) {
+			}
+			br.close();
+			System.out.println("Total number of Students: " + numStudents);
+			
+		} catch (Exception e) //using Exception rather than IOEcpetion 
+		{ 
 			System.out.println("Error check the file location");
-			E.printStackTrace();
-		} 
+			e.printStackTrace();
+		} //finally {}  copied from template online; not necessary in code 
 	}
 
 	public void parseFile(String line) {
-		String[] field = (line.replaceAll("\"", "") + "^^^^^^^^^^^^^^^^^^^end").split("\\^");
-		// System.out.println(line);
+		String[] field = (line + "^^^^^^end").split("\\^"); 
+		//no more replaceALL because we only have ^ in the text file
 		final String fName = field[0];
 		final String lName = field[1];
-		// System.out.println(fName);
-		// System.out.println(lName);
-
 		final int numericGrade = Integer.parseInt(field[2]);
-		String fullName = fName + " " + lName;
-		// System.out.println(fullName);
+		final String fullName = fName + " " + lName;
 
 		if (numericGrade >= 90 && numericGrade < 100) {
-			// System.out.println("A");
 			aList.add(fullName);
-		}
-		if (numericGrade >= 80 && numericGrade <= 89) {
-			// System.out.println("B");
+		} else if (numericGrade >= 80 && numericGrade <= 89) {
 			bList.add(fullName);
-		}
-		if (numericGrade <= 79 && numericGrade >= 75) {
-			// System.out.println("C");
+		} else if (numericGrade <= 79 && numericGrade >= 75) {
 			cList.add(fullName);
-		}
-		if (numericGrade >= 65 && numericGrade <= 74) {
-			// System.out.println("D");
+		} else if (numericGrade >= 65 && numericGrade <= 74) {
 			dList.add(fullName);
-		}
-		if (numericGrade >= 00 && numericGrade <= 64) {
-			// System.out.println("F");
+		} else if (numericGrade >= 00 && numericGrade <= 64) {
 			fList.add(fullName);
 		}
 	}
 
-	private void displayStudentGroup() {
-		System.out.println("Output 1");
-		System.out.println("----------------------------------------");
-		// output full name from each list
-		int countA = 0;
-		System.out.println("These are the A Students:");
-		for (String studentA : this.aList) {
-			countA++;
-			System.out.println(" " + countA + ".)  " + studentA);
-		}
-		System.out.println("----------------------------------------");
+	private static void countLetterGrade(ArrayList<String>gradeList) {
+		int count = 0;
+		//System.out.println("These are the " + gradeList.size() + " students with :" );
+		for (String student : gradeList) {
+			count++;
+			System.out.println(" " + count + ".)  " + student);
+		}		
+	} 
 
-		int countB = 0;
-		System.out.println("These are the B Students:");
-		for (String studentB : this.bList) {
-			countB++;
-			System.out.println(" " + countB + ".)  " + studentB);
-		}
-		System.out.println("----------------------------------------");
-
-		int countC = 0;
-		System.out.println("These are the C Students:");
-		for (String studentC : this.cList) {
-			countC++;
-			System.out.println(" " + countC + ".)  " + studentC);
-		}
-		System.out.println("----------------------------------------");
-
-		int countD = 0;
-		System.out.println("These are the D Students:");
-		for (String studentD : this.dList) {
-			countD++;
-			System.out.println(" " + countD + ".)  " + studentD);
-		}
-		System.out.println("----------------------------------------");
-
-		int countF = 0;
-		System.out.println("These are the F Students:");
-		for (String studentF : this.fList) {
-			countF++;
-			System.out.println(" " + countF + ".)  " + studentF);
-		}
-		System.out.println("----------------------------------------");
-		System.out.println("----------------------------------------");
+	public void displayStudentGroup() {
+		System.out.println("Students with A Grade: " + aList.size()  + "" );
+		countLetterGrade(aList);
+		System.out.println("-------------------------------------------------");
+		System.out.println("Students with B Grade: " + bList.size()+ "" );
+		countLetterGrade(bList);
+		System.out.println("-------------------------------------------------");
+		System.out.println("Students with C Grade: " + cList.size()+ "" );
+		countLetterGrade(cList);
+		System.out.println("-------------------------------------------------");
+		System.out.println("Students with D Grade: " + dList.size()+ "" );
+		countLetterGrade(dList);
+		System.out.println("-------------------------------------------------");
+		System.out.println("Students with F Grade: " + fList.size()+ "" );
+		countLetterGrade(fList);
 	}
 
 	public static void main(String[] args) {
-		GradesV1 obj = new GradesV1();
-		obj.readMe();
+		GradesV3 obj = new GradesV3();
+		obj.processFile();
 	}
 
 }
-
